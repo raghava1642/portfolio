e55807ee19d2381e8ea89ca4da5de2c4fe4b22c1
raghava1642 <21bq1a5455@vvit.net>
2025-07-06 23:15:40 +0530

    my personal portfolio

---
+++ script.js
@@ -0,0 +1,70 @@
+function changeAboutMyText() {
+  const aboutMeTexts = [
+    "Learner",
+    "Tech Enthusiast",
+    "Full Stack Web Developer" 
+  ];
+  const typingSpeed = 100;
+  const erasingSpeed = 50;
+  const pauseTime = 1500;
+  const aboutMeElement = document.querySelector(".about-me");
+
+  let textIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+
+  function type() {
+    const currentText = aboutMeTexts[textIndex];
+    /* Typing*/
+    if (!isDeleting && charIndex < currentText.length) {
+      aboutMeElement.textContent += currentText[charIndex];
+      charIndex++;
+      setTimeout(type, typingSpeed);
+    } else if (isDeleting && charIndex > 0) {
+    /* erasing*/
+      aboutMeElement.textContent = currentText.substring(0, charIndex - 1);
+      charIndex--;
+      setTimeout(type, erasingSpeed);
+    } else {
+      isDeleting = !isDeleting;
+      if (!isDeleting) {
+        textIndex = (textIndex + 1) % aboutMeTexts.length;
+      }
+      setTimeout(type, pauseTime);
+    }
+  }
+  type();
+}
+
+
+document.addEventListener('DOMContentLoaded',function(){
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const body = document.body;
+
+    darkModeToggle.addEventListener('click',()=>{
+        body.classList.toggle('dark-mode');
+        const currentMode = body.classList.contains('dark-mode')?'Dark' : 'Light';
+        darkModeToggle.querySelector('i').classList.toggle('fa-sun');
+        darkModeToggle.querySelector('i').classList.toggle('fa-moon');
+        darkModeToggle.querySelector('i').classList.toggle('light-mode');
+    });
+});
+changeAboutMyText();
+
+document.addEventListener('DOMContentLoaded', function () {
+    const observer = new IntersectionObserver(entries =>{
+        entries.forEach(entry => {
+            if (entry.isIntersecting){
+                const progressBar = entry.target.querySelector('.progress-bar');
+                const progress = progressBar.dataset.progress;
+                progressBar.style.setProperty('--progress', `${progress}%`);
+                progressBar.classList.add('animated');
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+    const programmingLanguages = document.querySelectorAll('#programming-languages .skill');
+    programmingLanguages.forEach(skill =>{
+        observer.observe(skill);
+    });
+});
